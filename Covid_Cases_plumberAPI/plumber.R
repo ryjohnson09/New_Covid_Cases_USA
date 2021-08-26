d66e3b31ce0706bca69a4f58ee03576d0b27bcab
ryjohnson09 <johnsrc06@gmail.com>
2021-08-26 19:32:48 +0000

    create pins and plumber APIs

---
+++ Covid_Cases_plumberAPI/plumber.R
@@ -0,0 +1,24 @@
+library(plumber)
+library(pins)
+library(modeltime)
+
+# Red in data ------------------------------------
+usa_total_cases <- pin_get("ryan/USA_totals_cases_deaths", board = "rsconnect") %>% 
+  select(Date, total_cases_avg_7)
+
+# Get model -------------------------------------
+cases_model <- pin_get("ryan/Covid_model_cases", board = "rsconnect")
+# coerce to mdl_time_tbl
+cases_model_1 <- structure(cases_model, class = c("mdl_time_tbl", "tbl_df", "tbl", "data.frame"))
+
+# API -------------------------------------------
+#* @apiTitle Predict 7-day average for new cases of Covid-19
+
+#* Predict new 7-day average
+#* @param pred_time Length of time to make predictions
+#* @get /echo
+function(pred_time) {
+  modeltime_forecast(cases_model1, h = pred_time,  actual_data = usa_total_cases)
+}
+
+
---
+++ Covid_Deaths_plumberAPI/plumber.R
@@ -0,0 +1,22 @@
+library(plumber)
+library(pins)
+library(modeltime)
+
+# Red in data ------------------------------------
+usa_total_deaths <- pin_get("ryan/USA_totals_cases_deaths", board = "rsconnect") %>% 
+  select(Date, total_deaths_avg_7)
+
+# Get model -------------------------------------
+deaths_model <- pin_get("ryan/Covid_model_deaths", board = "rsconnect")
+# coerce to mdl_time_tbl
+deaths_model_1 <- structure(deaths_model, class = c("mdl_time_tbl", "tbl_df", "tbl", "data.frame"))
+
+# API -------------------------------------------
+#* @apiTitle Predict 7-day average for new deaths of Covid-19
+
+#* Predict new 7-day average
+#* @param pred_time Length of time to make predictions
+#* @get /echo
+function(pred_time) {
+  modeltime_forecast(deaths_model_1, h = pred_time,  actual_data = usa_total_deaths)
+}
